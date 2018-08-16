87784a78f82f370138688359c268380eeab505a1
ligang7138 <445123875@qq.com>
2018-08-16 15:59:58 +0800

    优化v1版，使用一个channel做中转数据站

---
+++ v2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"go-websocket/v2/webLiveSocket"
+	"net/http"
+)
+
+var (
+	// http升级wobsocket的握手交互
+	upgrader = websocket.Upgrader{
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		ms   *webLiveSocket.WebLiveSocket
+		conn *websocket.Conn
+		err  error
+	)
+	if conn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		fmt.Println("升级websocket协议失败")
+		return
+	}
+	ms = webLiveSocket.InitMySocket(conn)
+	go ms.ReadLoop()
+	go ms.WirteLoop()
+}
+
+func main() {
+
+	http.HandleFunc("/ws", wsHandler)
+	http.ListenAndServe("0.0.0.0:7778", nil)
+}
---
+++ v2/webLiveSocket/liveSocket.go
@@ -0,0 +1,94 @@
+package webLiveSocket
+
+import (
+	"github.com/gorilla/websocket"
+	"sync"
+)
+
+type WebLiveSocket struct {
+	longCon      *websocket.Conn
+	transferData chan []byte
+	closeChan    chan byte
+	isClosed     bool
+	mutex        sync.Mutex
+}
+
+func InitMySocket(conn *websocket.Conn) (sk *WebLiveSocket) {
+	sk = &WebLiveSocket{
+		longCon:      conn,
+		transferData: make(chan []byte, 1024),
+		closeChan:    make(chan byte, 1),
+		isClosed:     false,
+	}
+	return
+}
+
+/*func (c *WebLiveSocket) ReadMessage() (data []byte, err error) {
+	select {
+	case data = <-c.inChan:
+	case <-c.closeChan:
+		err = errors.New("connection is close")
+	}
+	return
+}
+
+func (c *WebLiveSocket) SendMessage(data []byte) (err error) {
+	select {
+	case c. <-data:
+		fmt.Println("data enter outchan")
+	case <-c.closeChan:
+		err = errors.New("connection is closed")
+	}
+	return
+}*/
+
+func (c *WebLiveSocket) close() (err error) {
+	if err = c.longCon.Close(); err != nil {
+		return
+	}
+	c.mutex.Lock()
+	if !c.isClosed {
+		close(c.closeChan)
+		c.isClosed = true
+	}
+	c.mutex.Unlock()
+	return
+}
+
+func (c *WebLiveSocket) ReadLoop() {
+	for {
+		var (
+			err  error
+			data []byte
+		)
+		if _, data, err = c.longCon.ReadMessage(); err != nil {
+			goto ERR
+		}
+		select {
+		case c.transferData <- data:
+		case <-c.closeChan:
+			goto ERR
+		}
+	}
+ERR:
+	c.close()
+}
+
+func (c *WebLiveSocket) WirteLoop() {
+	var (
+		data []byte
+		err  error
+	)
+	for {
+		select {
+		case data = <-c.transferData:
+			if err = c.longCon.WriteMessage(websocket.TextMessage, data); err != nil {
+				goto ERR
+			}
+		case <-c.closeChan:
+			goto ERR
+		}
+	}
+ERR:
+	c.close()
+}
